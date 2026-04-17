03426bc4a180015e524c78d3f239b96c11626e0f
unknown <venkatsubramaniyan28@gmail.com>
2026-04-17 23:49:27 +0530

    tracing added

--- app.py
+++ app.py
@@ -1,42 +1,40 @@
+import os
 import uvicorn
+from dotenv import load_dotenv
 from fastapi import FastAPI, Request
+from langsmith import Client, tracing_context
+
 from src.graphs.graph_builder import GraphBuilder
 from src.llms.groqllm import GroqLLM
 
-import os
-from dotenv import load_dotenv
 load_dotenv()
 
-app=FastAPI()
+app = FastAPI()
 
-print(os.getenv("LANGCHAIN_API_KEY"))
-
-os.environ["LANGSMITH_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
-
-## API's
+ls_client = Client(
+    api_key=os.getenv("LANGSMITH_API_KEY"),
+    api_url="https://api.smith.langchain.com",
+)
 
 @app.post("/blogs")
-async def create_blogs(request:Request):
-    
-    data=await request.json()
-    topic= data.get("topic","")    
+async def create_blogs(request: Request):
+    data = await request.json()
+    topic = data.get("topic", "")
 
-    ## get the llm object
+    groqllm = GroqLLM()
+    llm = groqllm.get_llm()
 
-    groqllm=GroqLLM()
-    llm=groqllm.get_llm()
+    graph_builder = GraphBuilder(llm)
+    graph = graph_builder.setup_graph(usecase="topic")
 
-    ## get the graph
-    graph_builder=GraphBuilder(llm)
-    graph=graph_builder.setup_graph(usecase="topic")
-    state=graph.invoke({"topic":topic})
-    return {"data":state}
-    
-        
-    
+    with tracing_context(
+        enabled=True,
+        client=ls_client,
+        project_name="Blog Post Generator"
+    ):
+        state = graph.invoke({"topic": topic})
 
-    
+    return {"data": state}
 
-if __name__=="__main__":
-    uvicorn.run("app:app",host="0.0.0.0",port=8000,reload=True)
-
+if __name__ == "__main__":
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
